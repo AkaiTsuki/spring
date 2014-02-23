c00aa98d2b2926883b285aa037e0520b68f99d12
jiachiliu <jiachiliu@Jiachis-Computer.local>
2014-02-23 17:45:27 -0500

    Add rest sample

--- springmvcjacksonsample/src/main/java/springmvcsample/web/controller/JacksonController.java
+++ springmvcjacksonsample/src/main/java/springmvcsample/web/controller/JacksonController.java
@@ -1,6 +1,7 @@
 package springmvcsample.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,11 +14,20 @@ public class JacksonController {
 	/*
 	 * To return json, the @ResponseBody must be used on return type not method
 	 */
-	@RequestMapping(("/story"))
+	@RequestMapping("/story")
 	public @ResponseBody Story index() {
 		System.out.println("Jackson Controller");
 		Story story = new Story();
 		story.setId(1);
+		story.setContent("As a developer, I want to see this story as a json in web page");
+		return story;
+	}
+	
+	@RequestMapping("/rest/story/{id}")
+	public @ResponseBody Story restIndex(@PathVariable Integer id) {
+		System.out.println("Jackson Controller: restIndex");
+		Story story = new Story();
+		story.setId(id);
 		story.setContent("As a developer, I want to see this story as a json in web page");
 		return story;
 	}
